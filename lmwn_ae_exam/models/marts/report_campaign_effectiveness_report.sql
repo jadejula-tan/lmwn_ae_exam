cd012ab1bf6d8df1eabbc582264c0ce743de72c2
jtan7 <jtan@sertiscorp.com>
2025-06-02 00:01:29 +0700

    feat: Create mart report for campaign effectiveness report

---
+++ lmwn_ae_exam/models/marts/report_campaign_effectiveness_report.sql
@@ -0,0 +1,35 @@
+SELECT
+    cm.campaign_id,
+    cm.campaign_name,
+    cm.campaign_start_date,
+    cm.campaign_end_date,
+    cm.campaign_type,
+    cm.campaign_objective,
+    cm.campaign_channel,
+    cm.campaign_budget,
+    cm.cost_model,
+    cm.targeting_strategy,
+    -- cm.campaign_is_active,
+    cpd.campaign_interaction_date,
+    cpd.total_interactions,
+    cpd.total_clicks,
+    cpd.total_impressions,
+    cpd.total_conversions,
+    cpd.average_cost_per_click,
+    cpd.average_cost_per_impression,
+    cpd.average_cost_per_conversion,
+    cpd.average_revenue_per_conversion,
+    cpd.total_revenue,
+    cpd.total_ad_cost,
+    cpd.average_cost_per_new_customer,
+    cpd.return_on_ad_spend,
+    cpd.average_session_duration
+FROM
+    {{ ref('model_stg_campaigns') }} AS cm
+LEFT JOIN
+    {{ ref('model_int_campaign_performance_daily') }} AS cpd
+ON
+    cm.campaign_id = cpd.campaign_id
+    AND cpd.campaign_interaction_date BETWEEN cm.campaign_start_date AND cm.campaign_end_date
+WHERE
+    cm.campaign_is_active = TRUE
