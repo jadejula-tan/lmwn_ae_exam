f83db0223f9c9808fadd00f4509c5e11e984ecc2
jtan7 <jtan@sertiscorp.com>
2025-06-03 01:11:59 +0700

    feat: Create intermediate model for retargeting performance report

---
+++ lmwn_ae_exam/models/intermediate/model_int_campaign_retargeted_customer.sql
@@ -0,0 +1,59 @@
+
+WITH retargeted_cust_recurring AS (
+    SELECT
+        ci.campaign_id,
+        ci.customer_id,
+        COUNT(DISTINCT ot.order_id) AS num_recurring_orders,
+        SUM(ot.total_amount) AS total_purchase_amount,
+        MAX(
+            CASE 
+                WHEN ot.order_datetime > ci.interaction_datetime
+                     AND ot.order_datetime <= ci.interaction_datetime + INTERVAL '7 day'
+                THEN 1 ELSE 0 END
+        ) = 1 AS has_order_within_7_days,
+        MAX(
+            CASE 
+                WHEN ot.order_datetime > ci.interaction_datetime
+                     AND ot.order_datetime <= ci.interaction_datetime + INTERVAL '15 day'
+                THEN 1 ELSE 0 END
+        ) = 1 AS has_order_within_15_days,
+        MAX(
+            CASE 
+                WHEN ot.order_datetime > ci.interaction_datetime
+                     AND ot.order_datetime <= ci.interaction_datetime + INTERVAL '30 day'
+                THEN 1 ELSE 0 END
+        ) = 1 AS has_order_within_30_days
+    FROM
+        {{ ref('model_int_campaign_interactions_with_details') }} AS ci
+    LEFT JOIN
+        {{ ref('model_stg_campaigns') }} AS c
+    ON 
+        ci.campaign_id = c.campaign_id
+    LEFT JOIN
+        {{ ref('model_stg_customers') }} AS cus
+    ON 
+        ci.customer_id = cus.customer_id
+    LEFT JOIN
+        {{ ref('model_stg_orders') }} AS ot
+    ON
+        ci.customer_id = ot.customer_id
+        AND ci.interaction_datetime < ot.order_datetime
+        AND ci.order_id != ot.order_id
+    WHERE
+        LOWER(c.campaign_type) = 'retargeting'
+        AND (
+            (ci.is_new_customer = FALSE)
+            OR (lower(cus.status) = 'inactive')
+        )
+    GROUP BY
+        1,
+        2
+    ORDER BY
+        1,
+        2
+)
+SELECT
+    *,
+    num_recurring_orders > 0 AS is_recurring_customer
+FROM
+    retargeted_cust_recurring
---
+++ lmwn_ae_exam/models/intermediate/model_int_campaign_retargeted_customer_return_purchase.sql
@@ -0,0 +1,29 @@
+WITH customer_first_order AS (
+    SELECT
+        rc.customer_id,
+        MIN(o.order_datetime) AS first_order_datetime
+    FROM
+        {{ ref('model_int_campaign_retargeted_customer') }} AS rc
+    INNER JOIN
+            {{ ref('model_stg_orders') }} AS o
+    ON
+        rc.customer_id = o.customer_id
+    GROUP BY
+        1
+)
+SELECT
+    rc.campaign_id,
+    rc.customer_id,
+    cfo.first_order_datetime,
+    DATE_DIFF('day', cfo.first_order_datetime, ci.interaction_datetime) AS day_between_original_and_return_order
+FROM
+    {{ ref('model_int_campaign_retargeted_customer') }} AS rc
+INNER JOIN
+    {{ ref('model_int_campaign_interactions_with_details') }} AS ci
+ON
+    rc.campaign_id = ci.campaign_id
+    AND rc.customer_id = ci.customer_id
+LEFT JOIN
+    customer_first_order AS cfo
+ON
+    rc.customer_id = cfo.customer_id
