bad483d056f4c74cc1b013092afd8d6d04262a82
jtan7 <jtan@sertiscorp.com>
2025-06-04 15:16:18 +0700

    feat: Add driver feedback metrics to model_int_driver_feedback

--- lmwn_ae_exam/models/intermediate/model_int_driver_feedback.sql
+++ lmwn_ae_exam/models/intermediate/model_int_driver_feedback.sql
@@ -1,14 +1,48 @@
+WITH driver_sub_issue_ranked AS(
+    SELECT
+        driver_id,
+        issue_sub_type,
+        COUNT(1) AS sub_issue_count,
+        ROW_NUMBER() OVER (PARTITION BY driver_id ORDER BY COUNT(1) DESC) AS sub_issue_rank
+    FROM
+        {{ ref('model_stg_support_ticket') }}
+    WHERE
+        issue_type in ('rider', 'delivery')
+    GROUP BY
+        1,
+        2
+), driver_issues AS(
+    SELECT
+        driver_id,
+        ARRAY_AGG(issue_sub_type ORDER BY sub_issue_rank DESC) AS issue_raised_ranked
+    FROM
+        driver_sub_issue_ranked
+    GROUP BY
+        1
+), driver_aggregated AS(
+    SELECT
+        driver_id,
+        COUNT(DISTINCT ticket_id) AS total_tickets,
+        COUNT(DISTINCT CASE WHEN issue_sub_type = 'rude' THEN ticket_id END) AS count_rude_feedback,
+        COUNT(DISTINCT CASE WHEN issue_sub_type = 'no_mask' THEN ticket_id END) AS count_no_mask_feedback,
+        SUM(csat_score) AS total_csat_score,
+        AVG(csat_score) AS average_csat_score,
+        AVG(CASE WHEN issue_sub_type = 'rude' THEN csat_score ELSE NULL END) AS average_csat_score_rude,
+        AVG(CASE WHEN issue_sub_type = 'no_mask' THEN csat_score ELSE NULL END) AS average_csat_score_no_mask,
+        SUM(DATE_DIFF('minute', ticket_opened_datetime, ticket_resolved_datetime)) AS total_resolution_time_minutes
+    FROM
+        {{ ref('model_stg_support_ticket') }}
+    WHERE
+        issue_type in ('rider', 'delivery')
+    GROUP BY
+        1
+)
 SELECT
-    driver_id,
-    COUNT(DISTINCT ticket_id) AS total_tickets,
-    COUNT(DISTINCT CASE WHEN issue_sub_type = 'rude' THEN ticket_id END) AS count_rude_feedback,
-    COUNT(DISTINCT CASE WHEN issue_sub_type = 'no_mask' THEN ticket_id END) AS count_no_mask_feedback,
-    AVG(csat_score) AS average_csat_score,
-    AVG(CASE WHEN issue_sub_type = 'rude' THEN csat_score ELSE NULL END) AS average_csat_score_rude,
-    AVG(CASE WHEN issue_sub_type = 'no_mask' THEN csat_score ELSE NULL END) AS average_csat_score_no_mask
+    da.*,
+    di.issue_raised_ranked
 FROM
-    {{ ref('model_stg_support_ticket') }}
-WHERE
-    issue_type = 'rider'
-GROUP BY
-    1
+    driver_aggregated AS da
+LEFT JOIN
+    driver_issues AS di
+ON
+    da.driver_id = di.driver_id
